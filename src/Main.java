9d15aa89fa0a0491c12bffbc88e39f2bccd617ba
Wallace Wadge <wwadge@gmail.com>
2015-01-01 12:02:31 +0100

    Initial version

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+
+
+
+    public static int sum(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        // byte        -127 -> 127
+        // short          65535
+        // int          4M
+        // long
+
+
+        // boolean   true / false
+        // char       'a' , '1', '!'
+
+
+        // variable declaration:   type varname eg
+        // int i;
+
+
+        // get a good cheat sheet
+
+
+//        // loops:
+//        for (int i=0; i < 5; i++) {
+//
+//            System.out.println("Hello World!");
+//        }
+//
+//        while (!wheelsTurning && carMoving){
+//                // apply ABS brakes
+//        }
+//
+//        do{);
+//            // do something. This code will execute at least once
+//        } while(condition);
+
+        sum(3, 5);
+
+    }
+}
